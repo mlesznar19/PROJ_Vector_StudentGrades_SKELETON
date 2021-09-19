486ac13696743f33a8d6335a211283a00c39f006
Keith Perkins <keith.perkins@cnu.edu>
2021-09-19 17:59:03 -0400

    initial commit

---
+++ utilities/utilities.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdlib.h>
+#include <algorithm>
+
+#include "../includes/constants.h"
+#include "../includes/utilities.h"
+using namespace std;
+
+//if you are debugging the file must be in the project parent directory
+//in this case Project2 with the .project and .cProject files
+std::string DoubleToString(double Number) {
+	std::ostringstream ss;
+	ss << Number;
+	return ss.str();
+}
+
+//if myString does not contain a string rep of number returns o
+//if int not large enough has undefined behaviour, very fragile
+int stringToInt(const char *myString) {
+	return atoi(myString);
+}
