7ad8ea3c5b18087de69091827c6ccf8cff45ccd5
Michael Lesznar <michael.lesznar.19@cnu.edu>
2021-09-30 15:23:49 -0400

    defining read func

--- utilities/utilities.cpp
+++ utilities/utilities.cpp
@@ -35,3 +35,59 @@ double stringToDouble(const char *myString) {
 	return atof(myString);
 }
 
+/***
+ * Clears allStudentData, Opens file, For each line; parses name, midterm1,midterm2 and an
+ * optional finalgrade (if present in file)from that line. Puts this info into a studentData struct,
+ * adds this struct to allstudentData vector.
+ * NOTE:
+ * Ignore empty lines
+ * Non empty lines will contain 4 or 5 values to be parsed.
+ * the default param separator_char is defined in the header file declaration of readFile ONLY, it is not in the readFile definition.
+ * @param file - where student data is stored
+ * @param allstudentData - a vector that holds the student data info after it has been read from file
+ * @param separator_char - the character that separates each bit of data in a line of student data
+ * @return COULD_NOT_OPEN_FILE
+ *         SUCCESS
+ */
+int readFile(std::string &file, std::vector<KP::studentData> &allstudentData, char separator_char) {
+
+
+	separator_char = KP::SEPERATOR_CHAR;
+	//clear the vector
+	allstudentData.clear();
+
+	std::string line;
+	std::string token;
+	KP::studentData myStudentData;
+	stringstream ss;
+	ifstream myInputFile;
+
+	//opens the file
+	myInputFile.open(file.c_str(), ios::in);
+
+	while (!myInputFile.eof()){
+
+		//get a line from the file (name, midterm1, midterm2, possibly final grade
+		getline(myInputFile, line);
+		ss.str(line);
+
+		myStudentData.clear();
+
+		//get the name
+		getline(ss, myStudentData.name, KP::SEPERATOR_CHAR);
+
+		//get midterm1
+		getline(ss, token, KP::SEPERATOR_CHAR);
+		myStudentData.midterm1 = stringToInt(token.c_str());
+
+		//get midterm2
+		getline(ss, token, KP::SEPERATOR_CHAR);
+		myStudentData.midterm2 = stringToInt(token.c_str());
+
+		//add the student data to vector
+		allstudentData.push_back(myStudentData);
+	}
+	return KP::SUCCESS;
+}
+
+
